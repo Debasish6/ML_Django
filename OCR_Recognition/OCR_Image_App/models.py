9a61415a5c871ecbd903527e5f43a48700b7625f
Debasish6 <iamdebasishghosh6@gmail.com>
2024-09-09 09:29:33 +0530

    Design Updated Successfully

--- OCR_Recognition/OCR_Image_App/models.py
+++ OCR_Recognition/OCR_Image_App/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Document(models.Model):
-    image = models.ImageField(upload_to='static/img/')
+    image = models.ImageField(upload_to='OCR_Image_App/static/img/')
+    processed_image = models.ImageField(upload_to='OCR_Image_App/static/processed_images/', blank=True, null=True)
     text = models.TextField(blank=True, null=True)
--- OCR_Recognition/OCR_Image_App/urls.py
+++ OCR_Recognition/OCR_Image_App/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.ocr_image_upload, name='ocr_image_upload'),
-    # path('ocr_text/<int:pk>',views.ocr_text, name='ocr_text'),
 ]
